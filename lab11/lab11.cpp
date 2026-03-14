e97028cbed0969bacdf9a73661a09de99245b6ef
Khanh Huy <caohuylk9@gmail.com>
2026-03-14 23:01:24 +0700

    add lab1_1.cpp

--- lab11/lab11.cpp
+++ lab11/lab11.cpp
@@ -1,41 +1,41 @@
-//
-//#include <iostream>
-//using namespace std;
-//struct PhanSo
-//{
-//	long long TuSo, MauSo;
-//};
-//void RutGon(PhanSo& x)
-//{
-//	long long m = x.TuSo;
-//	long long n = x.MauSo;
-//	while (n != 0)
-//	{
-//		long long temp = m % n;
-//		m = n;
-//		n = temp;
-//	}
-//	x.TuSo /= m;
-//	x.MauSo /= m;
-//	if (x.MauSo < 0)
-//	{
-//
-//		x.TuSo = -x.TuSo;
-//		x.MauSo = -x.MauSo;
-//	}
-//}
-//int main()
-//{
-//	PhanSo a;
-//	cin >> a.TuSo >> a.MauSo;
-//	if (a.TuSo == 0) {cout << 0;return 0;}
-//  if (a.MauSo == 0) return 0;				// nếu mẫu bằng 0 thì không in ra gì
-//	RutGon(a);
-//	if (a.MauSo == 1) cout << a.TuSo;
-//	else cout << a.TuSo << "/" << a.MauSo;
-//	return 0;
-//
-//}
+
+#include <iostream>
+using namespace std;
+struct PhanSo
+{
+	long long TuSo, MauSo;
+};
+void RutGon(PhanSo& x)
+{
+	long long m = x.TuSo;
+	long long n = x.MauSo;
+	while (n != 0)
+	{
+		long long temp = m % n;
+		m = n;
+		n = temp;
+	}
+	x.TuSo /= m;
+	x.MauSo /= m;
+	if (x.MauSo < 0)
+	{
+
+		x.TuSo = -x.TuSo;
+		x.MauSo = -x.MauSo;
+	}
+}
+int main()
+{
+	PhanSo a;
+	cin >> a.TuSo >> a.MauSo;
+	if (a.TuSo == 0) {cout << 0;return 0;}
+  if (a.MauSo == 0) return 0;				// nếu mẫu bằng 0 thì không in ra gì
+	RutGon(a);
+	if (a.MauSo == 1) cout << a.TuSo;
+	else cout << a.TuSo << "/" << a.MauSo;
+	return 0;
+
+}
 
 
 
